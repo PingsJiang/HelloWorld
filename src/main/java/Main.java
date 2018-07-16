33e44216f599029734aedcf708375e89ceaea3be
jiangpingsheng <jiangpingsheng@sina.cn>
2018-07-16 23:07:04 +0800

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,17 @@
+import org.apache.ibatis.jdbc.SQL;
+
 public class Main {
+
+    public String get()
+    { return new SQL(){{
+            SELECT();
+            FROM();
+            WHERE();
+            if (true){
+
+            }
+        }
+    }.toString();}
 
     public static void main(String[] args) {
 
